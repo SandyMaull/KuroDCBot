fb823aa31c276f3271079c6d1adea075f30ea7a0
SandyMaull <sandymaulana0@gmail.com>
2021-09-28 16:08:14 +0800

    First Commit

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ cogs = [music]
 client = commands.Bot(command_prefix='?', intents = discord.Intents.all())
 
 for i in range(len(cogs)):
-  cogs[i].setup(client)
+    cogs[i].setup(client)
 
 @client.event
 async def on_ready():
-  print("{0.user} successful log in! ".format(client))
+    print("{0.user} successful log in! ".format(client))
 
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
-  msg = message.content
-  if msg.startswith('$hello'):
-    await message.channel.send('Hello {user} ! \nHow r u today? \n'.format(user=message.author))
-  await client.process_commands(message)
+    if message.author == client.user:
+        return
+    msg = message.content
+    if msg.startswith('$hello'):
+        await message.channel.send('Hello {user} ! \nHow r u today? \n'.format(user=message.author))
+    await client.process_commands(message)
 
 client.run(os.getenv('TOKEN'))
